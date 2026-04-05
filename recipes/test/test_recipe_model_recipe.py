c77f3fc61c00f314646c2a2597900b31f13f2d84
Lucas Dos Santos Nascimento <lucas.nascimento.engineer@gmail.com>
2026-04-04 21:18:26 -0300

    testando os max_lenght

--- recipes/test/test_recipe_model_recipe.py
+++ recipes/test/test_recipe_model_recipe.py
@@ -1,5 +1,5 @@
 from . test_recipe_base import RecipeTestBase
-
+from parameterized import parameterized
 from django.core.exceptions import ValidationError
 
 
@@ -7,11 +7,15 @@ class RecipeModelTest(RecipeTestBase):
     def setUp(self):
         self.recipe = self.make_recipe()
         return super().setUp()
-    
-    def test_recipe_tilte_raises_error_if_title_has_more_than_65_chars(self):
-        self.recipe.title = 'A' * 70
-        
+
+    @parameterized.expand([
+        ("title", 65),
+        ("description", 165),
+        ("preparation_time_unit", 65),
+        ("servings_unit", 65),
+    ])
+    def tets_recipe_fields_max_lenght(self, field, max_lenght):
+        setattr(self.recipe, field, "A", * (max_lenght + 0))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
-
-        
+            

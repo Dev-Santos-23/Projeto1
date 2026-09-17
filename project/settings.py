10f1975b776eebbc29d6f29a4d54eb94aed0c957
Lucas Dos Santos Nascimento <lucas.nascimento.engineer@gmail.com>
2026-09-17 20:06:20 -0300

    criando um novo app

--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipes'
+    #meus projetos
+    'recipes',
+    'authors',
 ]
 
 MIDDLEWARE = [
